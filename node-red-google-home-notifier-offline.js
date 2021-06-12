041c13eef10108dd77a450f52aeead93d2fcbf3e
Orcé MARINKOVSKI <Orc MARINKOVSKI>
2021-06-12 09:01:23 +0200

    code refactoring

--- node-red-google-home-notifier-offline.js
+++ node-red-google-home-notifier-offline.js
@@ -10,7 +10,7 @@ var httpServer = "";
 
 var ip = require("ip");
 var serverIP = ip.address();
-var serverPort = "8081"; // default port for serving mp3
+var serverPort = "8089"; // default port for serving mp3
 const cacheFolder ="/tmp"
 var listOfDevices = [];
 
@@ -19,14 +19,11 @@ module.exports = function (RED) {
 
   // Configuration node
   function GoogleHomeConfig(n) {
-    localFileServerRestart();
+    // localFileServerRestart();
 
     RED.nodes.createNode(this, n);
 
     persistUserDeviceConfigs(n);
-    this.ipaddress = n.ipaddress;
-    this.language = n.language;
-    this.name = n.name;
 
     //Prepare language Select Box
     var obj = require('./languages');
@@ -47,7 +44,16 @@ module.exports = function (RED) {
     });
 
     //Known issue: when 'language' is Default/Auto, this will fail & return undefined
-    this.googlehomenotifier = require('google-home-notifier-offline')(this.ipaddress, this.language, 1,serverIP,serverPort,cacheFolder);
+    serverPort=(n.mediaListenPort?n.mediaListenPort:8089);
+    this.googlehomenotifier = require('google-home-notifier-offline')(
+      n.ipaddress, 
+      n.language, 
+      1,
+      serverIP,
+      serverPort,
+      cacheFolder,
+      (n.notificationLevel?n.notificationLevel/100:0.2)
+      );
 
     //Build another API to auto detect IP Addresses
     discoverIpAddresses('googlecast', function (ipaddresses) {
@@ -112,25 +118,48 @@ module.exports = function (RED) {
       //     });
       //   });
       // })
+      
+      if (msg.emitVolume){
+        config.googlehomenotifier.setEmitVolume(msg.emitVolume);
+      }
+      if (msg.speed){
+        config.googlehomenotifier.setSpeechSpeed(msg.speed);
+      }
+
+      node.status({
+        fill: "blue",
+        shape: "dot",
+        text: "preparing voice message"
+      });
+
       config.googlehomenotifier
-        .setSpeechSpeed(msg.speed||1)
-        .setEmitVolume(msg.emitVolume)
         // .setFileServerPort(msg.fileServerPort===undefined?"":msg.fileServerPort)
         // .setCacheFolder(msg.cacheFolder===undefined?"":msg.cacheFolder)
-        .notify(msg.payload, function (result) {
-              node.status({
-                fill: "green",
-                shape: "ring",
-                text: "Successfully sent voice command"
-                });
-          });
+        .notify(msg.payload)
+        .then(_ =>
+          node_status_ready()
+          )
+          .catch(e => 
+            node.status(node.status({
+              fill:"red",
+              shape:"ring",
+              text:e
+            })));
     });
 
-    config.googlehomenotifier.on('error', function (error) {
+    config.googlehomenotifier.on('status', function (message) {
+      node.status({
+        fill: "blue",
+        shape: "dot",
+        text: message
+      });
+    }),
+
+    config.googlehomenotifier.on('error', function (message) {
       node.status({
         fill: "red",
         shape: "ring",
-        text: error.message
+        text: message
       });
     });
 
@@ -144,91 +173,99 @@ module.exports = function (RED) {
       return;
     }
 
-    node.status({
-      fill: "blue",
-      shape: "dot",
-      text: "ready"
-    });
+    node_status_ready();
+
     config.googlehomenotifier.setMaxListeners(Infinity);
+
+    function node_status_ready() {
+      node.status({
+        fill: "green",
+        shape: "dot",
+        text: "ready"
+      });
+    }
   };
 
   RED.nodes.registerType("googlehome-notifier-offline", GoogleHomeNotifier);
 
 
-};
-function persistUserDeviceConfigs(n) {
-  const updatedDevice = listOfDevices.find(device => device.id === n.id);
-  if (updatedDevice) {
-    listOfDevices.forEach(device => {
-      if (device.id === n.id) {
-        device = n;
-      }
-    });
-  } else {
-    listOfDevices.push(n);
+  function persistUserDeviceConfigs(n) {
+    const updatedDevice = listOfDevices.find(device => device.id === n.id);
+    if (updatedDevice) {
+      listOfDevices.forEach(device => {
+        if (device.id === n.id) {
+          device = n;
+        }
+      });
+    } else {
+      listOfDevices.push(n);
+    }
   }
-}
-function discoverIpAddresses(serviceType, discoveryCallback) {
-  var ipaddresses = [];
-  var bonjour = require('bonjour')();
-  var browser = bonjour.find({
-    type: serviceType
-  }, function (service) {
-    service.addresses.forEach(function (element) {
-      if (element.split(".").length == 4) {
-        var label = "" + service.txt.md + " (" + element + ")";
-        ipaddresses.push({
-          label: label,
-          value: element
-        });
-      }
-    });
-
-    //Add a bit of delay for all services to be discovered
-    if (discoveryCallback)
-      setTimeout(function () {
-        discoveryCallback(ipaddresses);
-      }, 2000);
-  });
-}
-
-function localFileServerClose(callback) {
-  if (httpServer !== "") {
-    httpServer.close(function () {
-      httpServer = "";
+  function discoverIpAddresses(serviceType, discoveryCallback) {
+    var ipaddresses = [];
+    var bonjour = require('bonjour')();
+    var browser = bonjour.find({
+      type: serviceType
+    }, function (service) {
+      service.addresses.forEach(function (element) {
+        if (element.split(".").length == 4) {
+          var label = "" + service.txt.md + " (" + element + ")";
+          ipaddresses.push({
+            label: label,
+            value: element
+          });
+        }
+      });
+  
+      //Add a bit of delay for all services to be discovered
+      if (discoveryCallback)
+        setTimeout(function () {
+          discoveryCallback(ipaddresses);
+        }, 2000);
     });
   }
-  callback();
-}
-
-function localFileServerStart() {
-
-  const FileServer = require('file-server');
-
-  const fileServer = new FileServer((error, request, response) => {
-    response.statusCode = error.code || 500;
-    response.end("404: Not Found " + request.url);
-  });
-
-  const serveRobots = fileServer.serveDirectory(cacheFolder, {
-    '.mp3': 'audio/mpeg'
-  });
-
-  httpServer = require('http')
-    .createServer(serveRobots)
-    .listen(serverPort);
-  console.log("fileServer listening on ip " + serverIP + " and port " + serverPort);
-
-}
-
-function localFileServerRestart() {
-  if (httpServer === "") {
-    localFileServerStart();
-  } else {
-    localFileServerClose(function () {
+  
+  function localFileServerClose(callback) {
+    if (httpServer !== "") {
+      httpServer.close(function () {
+        httpServer = "";
+      });
+    }
+    callback();
+  }
+  
+  function localFileServerStart() {
+  
+    const FileServer = require('file-server');
+  
+    const fileServer = new FileServer((error, request, response) => {
+      response.statusCode = error.code || 500;
+      response.end("404: Not Found " + request.url);
+    });
+  
+    const serveRobots = fileServer.serveDirectory(cacheFolder, {
+      '.mp3': 'audio/mpeg'
+    });
+  
+    httpServer = require('http')
+      .createServer(serveRobots)
+      .listen(serverPort);
+    console.log("fileServer listening on ip " + serverIP + " and port " + serverPort);
+  
+  }
+  
+  function localFileServerRestart() {
+    if (httpServer === "") {
       localFileServerStart();
-    })
+    } else {
+      localFileServerClose(function () {
+        localFileServerStart();
+      })
+    }
   }
-}
+  localFileServerStart();
+
+};
+
 
 
